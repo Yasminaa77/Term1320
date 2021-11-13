aabe66fc672df401cd9ad235332d11999c2b277b
Yasmina Amir <66489192+Yasminaa77@users.noreply.github.com>
2021-11-12 17:55:54 -0800

    first milestone commit

--- IOhandler.js
+++ IOhandler.js
@@ -14,6 +14,7 @@ const unzipper = require('unzipper'),
   path = require('path');
 
 
+
 /**
  * Description: decompress file from given pathIn, write to given pathOut 
  *  
@@ -25,15 +26,26 @@ const unzipper = require('unzipper'),
 
 
 //-----------------------1--------------------------
-//---Unzip the using unzipper library
+//---Unzip the using unzipper library (streams)
     //"Extraction operation complete"
     //create a directory called unzipped with all your images in it.(1,2,3)
+    //make an array of each photo path
+    //sending it to greyscale function
 
+// let unzippedPath = __dirname + "\\myfile.zip";
+// let zippedPath= 'myfiles';
+//https://www.npmjs.com/package/unzipper
 
 const unzip = (pathIn, pathOut) => {
-
+ return fs.createReadStream(pathIn)
+    .pipe(unzipper.Extract({path: pathOut}))
+    .promise()
 };
+
+
+
 //https://github.com/Yasminaa77/Term1320.git
+
 
 /**
  * Description: read all the png files from given directory and return Promise containing array of each png file path 
@@ -41,9 +53,28 @@ const unzip = (pathIn, pathOut) => {
  * @param {string} path 
  * @return {promise}
  */
-const readDir = dir => {
 
-};
+////creating array of the png images paths
+
+
+const readDir = dir => {
+    let images=[];
+    return new Promise((resolve, reject) => {
+        fs.readdir(dir , function(err, data){
+            if (err){
+                reject(err)
+            }else{
+                for(let image of data ){
+                    if( path.extname(image) === ".png"){
+                        images.push(image);
+                    }
+                }
+               resolve(images);
+            }
+        });
+    })
+}
+
 
 /**
  * Description: Read in png file by given pathIn, 
@@ -58,22 +89,55 @@ const readDir = dir => {
 
 //-----------------------2--------------------------
 //----grey scale function (parameters: pathIn and pathOut.)
-    //PNGJS
+    //PNGJS will be used to parse the path in images (giving an array
+    //loop through all three arrays of px and change the values
+    //save the modified as png  one and pass it to path (new directory)
 
 const grayScale = (pathIn, pathOut) => {
 
-};
+// let images=[ 'in.png', 'in1.png', 'in2.png' ]
+
+        // images.forEach((pngImage)=> {
+        //     console.log("im working")
+            fs.createReadStream(`${pathIn}`)
+                .pipe(
+                    new PNG({})
+                )
+
+
+                .on("parsed", function () {
+                    let  grey;
+                    let  random;
+                    for (let y = 0; y < this.height; y++) {
+                        for (let x = 0; x < this.width; x++) {
+                            let idx = (this.width * y + x) << 2;
+
+
+                            grey = (this.data[idx] + this.data[idx + 1]+ this.data[idx + 2])/3
+
+                            this.data[idx] = grey ;
+                            this.data[idx + 1] = grey;
+                            this.data[idx + 2] = grey;
+
+                        }
+                    }
+                    random= Math.floor(Math.random()*1000);
+                    this.pack().pipe(fs.createWriteStream(`${pathOut}/edited${random}.png`));
+                });
+
+
+        // })
+
+    };
+
+// grayScale(`${__dirname}/unzipped`,`${__dirname}/grayscaled`)
 
 module.exports = {
   unzip,
   readDir,
-  grayScale
+    grayScale
 };
 
-
-
-
-
-
+//status, add ., commit -m "comment", push -u origin master
 //.gitignore
 
--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 /*
  * Project: COMP1320 Milestone 1
  * File Name: main.js
- * Description: 
- * 
- * Created Date: 
- * Author:
- * 
+ * Description:
+ *
+ * Created Date:
+ * Author: Yasmina
+ *
  */
 
 const IOhandler = require("./IOhandler"),
@@ -13,11 +13,19 @@ const IOhandler = require("./IOhandler"),
   pathUnzipped = `${__dirname}/unzipped`,
   pathProcessed = `${__dirname}/grayscaled`;
 
+const  unzip= require("./IOhandler").unzip
+const  readDir= require("./IOhandler").readDir
+const  grayScale= require("./IOhandler").grayScale
 
 
-
-
-
-
+unzip(zipFilePath, pathUnzipped)
+    .then(() => console.log("Extraction operation complete"))
+    .then(() => readDir("./unzipped"))
+    .then((images) => { // [in1.png in2.png]
+        images.forEach((img) => {
+            grayScale(`${__dirname}/unzipped/${img}`,`${__dirname}/grayscaled`)
+        })
+    })
+    .catch((err) => console.log(err))
 
 
